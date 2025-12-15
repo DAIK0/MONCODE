3cd6e6ee8a5536ce3e327329290b106b49ed6bf8
DAIK0 <usuario00.exe@gmail.com>
2025-12-15 13:36:49 -0600

    ultimos cambios

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/user.routes.js';
 //importamos las rutas para productos
 import productRoutes from './routes/products.routes.js'
 import orderRoutes from './routes/order.routes.js';
+import { verify } from 'jsonwebtoken';
 
 const app = express();
 app.use(cors({
@@ -25,5 +26,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/', authRoutes)
 app.use('/api/', productRoutes);
 app.use('/api/', orderRoutes);
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Bienbendo a la api del meñotote',
+        version: '1.0.0',
+        rutasDisponibles: [
+            { endpoint: '/api/register', metodo: 'POST', descripcion: 'Registrar un nuevo usuario' },
+            { endpoint: '/api/login', metodo: 'POST', descripcion: 'Iniciar sesión de usuario' },
+            { endpoint: '/', metodo: 'GET', descripcion: 'Ruta inicial de la aplicacion' }
 
+        ]
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'UP', message: 'El servidor está funcionando correctamente.' });
+});
+
+//Manejo de errores para rutas no encontradas
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Ruta no encontrada' });
+});
 export default app;
